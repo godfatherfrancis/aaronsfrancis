2fb846d1852f2abb20cda60079231a8890d23149
godfrancisfrancis <francis.aarons@gmail.com>
2017-11-15 20:36:10 -0500

    background glitch enabled

--- script/js/custom.js
+++ script/js/custom.js
@@ -1,22 +1,5 @@
 jQuery( document ).ready( function( $ ){
 
-    printCredits();
-
-	// Intro size setup
-
-	setupIntro(); // On ready, resize intro
-
-	$( window ).resize( function() { setupIntro(); } ); // On resize
-
-	function setupIntro() {
-		var infoBox = $('.info' );
-		var titleBox = $( '.info').find('.container');
-		$( '.info' ).find( '.container' ).css( 'top', ( ( infoBox.height() - titleBox.height() - 20 ) / 1.87) + 'px' );
-	}
-
-	// Full background image
-	$( '.fx-backstretch' ).find( '.info' ).backstretch( 'images/water.jpg' );
-
     function printCredits() {
         console.log("                                                                                          \
             \n     ______          ______      __  __                 ______                      _            \
@@ -32,5 +15,36 @@ jQuery( document ).ready( function( $ ){
             \n  >Twitter: @godfathrfrancis                                                                     \
             ");
     }
+
+    printCredits();
+
+	// Intro size setup
+
+	// setupIntro(); // On ready, resize intro
+
+	// $( window ).resize( function() { setupIntro(); } ); // On resize
+
+	function setupIntro() {
+		var infoBox = $('.info' );
+		var titleBox = $( '.info').find('.container');
+		$( '.info' ).find( '.container' ).css( 'top', ( ( infoBox.height() - titleBox.height() - 20 ) / 1.87) + 'px' );
+	}
+
+    function glitch() {
+        $( ".glitch-img" ).mgGlitch({
+            destroy : false, // set 'true' to stop the plugin
+            glitch: true, // set 'false' to stop glitching
+            scale: true, // set 'false' to stop scaling
+            blend : true, // set 'false' to stop glitch blending
+            blendModeType : 'hue', // select blend mode type
+            glitch1TimeMin : 200, // set min time for glitch 1 elem
+            glitch1TimeMax : 400, // set max time for glitch 1 elem
+            glitch2TimeMin : 10, // set min time for glitch 2 elem
+            glitch2TimeMax : 100, // set max time for glitch 2 elem
+            // zIndexStart : 8, // because of absolute position, set z-index base value
+        });
+    }
+
+    glitch();
 
 });
